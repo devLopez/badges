9a24f732c4c100aaabca740d461b30d98061f88a
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2017-09-02 18:33:22 -0300

    Criacao do Service Provider para melhor suporte ao LaravelOC

---
+++ src/Providers/BadgesServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Igrejanet\Badges\Providers;
+
+use Igrejanet\Badges\Badge;
+use Igrejanet\Badges\Contracts\BadgeContract;
+use Igrejanet\Badges\Contracts\MembersContract;
+use Igrejanet\Badges\Person\Members;
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+use Knp\Snappy\Pdf;
+
+/**
+ * BadgesServiceProvider
+ *
+ * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @since   02/09/2017
+ * @package Igrejanet\Badges\Providers
+ */
+class BadgesServiceProvider extends ServiceProvider
+{
+    /**
+     * @var bool
+     */
+    protected $defer = true;
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        $this->app->bind(BadgeContract::class, function() {
+            $binary = env('WKHTMLTOPDF_LOCATION');
+            $pdf    = new Pdf($binary);
+
+            return new Badge($pdf);
+        });
+
+        $this->app->bind(MembersContract::class, function() {
+            $collection = new Collection();
+
+            return new Members($collection);
+        });
+    }
+}
