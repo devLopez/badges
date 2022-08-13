833cef0cba03534145e02238d403ca2724b94760
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-08-12 21:38:49 -0300

    Changed CompanyTest to pest

--- tests/CompanyTest.php
+++ tests/CompanyTest.php
@@ -1,31 +1,27 @@
 <?php
 
-    use Igrejanet\Badges\Person\Company;
+use Igrejanet\Badges\Person\Company;
 
-    use PHPUnit\Framework\TestCase;
+it('should test instance info', function () {
+    $logo = 'logo.png';
+    $type = 'Carteira de Identificação Ministerial';
 
-    class CompanyTest extends TestCase
-    {
-        public function testInstance()
-        {
-            $logo        = 'logo.png';
-            $type        = 'Carteira de Identificação Ministerial';
-            $companyInfo = [
-                'II Igreja de Deus do Avivamento Bíblico',
-                'Rua G, 336 - Vila Campos - Montes Claros - MG',
-                'Tel.: (38)4009-5777 - idabmoc@gmail.com.br - http://idabmoc.com'
-            ];
-            $cardInfo = [
-                'Uso exclusivo para identificação ministerial',
-                'Esta carteira é pessoal e intransferível',
-                'Válida somente enquanto o usuário estiver regularmente registrado'
-            ];
+    $companyInfo = [
+        'II Igreja de Deus do Avivamento Bíblico',
+        'Rua G, 336 - Vila Campos - Montes Claros - MG',
+        'Tel.: (38)4009-5777 - idabmoc@gmail.com.br - http://idabmoc.com',
+    ];
 
-            $company = new Company($logo, $type, $companyInfo, $cardInfo);
+    $cardInfo = [
+        'Uso exclusivo para identificação ministerial',
+        'Esta carteira é pessoal e intransferível',
+        'Válida somente enquanto o usuário estiver regularmente registrado',
+    ];
 
-            $this->assertInstanceOf(Company::class, $company);
-            $this->assertEquals($type, $company->type);
-            $this->assertEquals($companyInfo, $company->companyInfo);
-            $this->assertEquals($cardInfo, $company->cardInfo);
-        }
-    }
+    $company = new Company($logo, $type, $companyInfo, $cardInfo);
+
+    $this->assertInstanceOf(Company::class, $company);
+    $this->assertEquals($type, $company->type);
+    $this->assertEquals($companyInfo, $company->companyInfo);
+    $this->assertEquals($cardInfo, $company->cardInfo);
+});
